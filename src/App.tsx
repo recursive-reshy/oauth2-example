223d148e60f390a5a8730c7f0a19763392b014f8
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-06-17 17:44:01 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="dark dark:bg-black dark:text-white">
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
