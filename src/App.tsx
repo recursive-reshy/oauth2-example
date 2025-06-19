e5e9674028d1f20e97b92ff963f8da7c7b510ed7
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-06-19 13:20:40 +0800

    App.tsx: provide theme context to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+// Contexts
+import { ThemeProvider } from "./context"
+
 // Pages
 import { PageLogin } from "./pages"
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PageLogin />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider
+      defaultTheme="dark"
+      storageKey="oauth-eg-ui-theme"
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<PageLogin />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
