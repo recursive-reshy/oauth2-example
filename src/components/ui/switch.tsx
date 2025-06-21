8901556c434cc3eb89cd45e798327352f7754d37
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-06-21 17:28:14 +0800

    components\ui\switch.tsx: Refactored to use named export

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -1,12 +1,12 @@
 import { type FC } from "react"
-import * as SwitchPrimitive from "@radix-ui/react-switch"
+import { Root, Thumb } from "@radix-ui/react-switch"
 
 import { cn } from "@/lib/utils"
 
 // Icons
 import { Icon, type Icon as IconType } from "@/components"
 
-interface SwitchProps extends React.ComponentProps< typeof SwitchPrimitive.Root > {
+interface SwitchProps extends React.ComponentProps< typeof Root > {
   checkedIcon?: IconType
   unCheckedIcon?: IconType
 }
@@ -18,7 +18,7 @@ const Switch: FC< SwitchProps > = ( {
   ...props
 } ) => {
   return (
-    <SwitchPrimitive.Root
+    <Root
       data-slot="switch"
       className={cn(
         "peer data-[state=checked]:bg-primary data-[state=unchecked]:bg-input focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[state=unchecked]:bg-input/80 inline-flex h-[1.15rem] w-9 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
@@ -40,14 +40,14 @@ const Switch: FC< SwitchProps > = ( {
           size="small"
         />
       }
-      <SwitchPrimitive.Thumb
+      <Thumb
         data-slot="switch-thumb"
         className={cn(
           "bg-background dark:data-[state=unchecked]:bg-foreground dark:data-[state=checked]:bg-primary-foreground pointer-events-none block size-4 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-4.5 data-[state=unchecked]:translate-x-0"
         )}
       />
-    </SwitchPrimitive.Root>
+    </Root>
   )
 }
 
-export { Switch }
+export default Switch
